34708e19aa1d27a8e84bc8d607d8d19ec7e5f030
JSlovak <jasmine.slovak@gmail.com>
2017-03-15 19:39:16 -1000

    Clean version. Exercise Completed, All tests passed.

--- vault.js
+++ vault.js
@@ -1,27 +1,12 @@
 'use strict';
 
-// Create a module that exposes 2 functions.
-// work on the `vault.js` file.
-// make tests pass
-
 var vaultGen =(function (){
 
   var vault = {};
 
-//   ##### setValue
-// setValue( key, value )
-// accepts two arguments `key` and `value`.
-// will set the `value` in the vault identified by it's `key`.
-
   var setValue = function(key, precious){
     vault[key] = precious;
   };
-
-//   ##### getValue
-// getValue( key )
-// accepts one argument `key`.
-// will return the `value` that was set if the `key` exists in the vault.
-// will return `null` if the `key` does not exist in the vault.
 
   var getValue = function(key){
     if (!vault[key]){
@@ -39,11 +24,4 @@ var vaultGen =(function (){
 });
 
 var myVault = vaultGen();
-
-// ### Example usage
-// console.log(myVault);
-// console.log(myVault.setValue("aluminium","MONEY!"));
-// console.log(myVault.getValue("aluminium"));
-// console.log(myVault.getValue("somethingElse"));
-
 module.exports = vaultGen;
